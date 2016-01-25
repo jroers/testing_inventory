e9a9ca4c356f04aab7ee9e1096bd063c73c511cd
jroers <joe.roers@gmail.com>
2016-01-25 15:26:19 -0800

    passed the first two tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+		product_info = Hash.new
+
+		wholesale_price = Random.new.rand(1.0..100.0).round(2)
+    
+    product_info[:name] = FFaker::Lorem.words(5).join(" ")
+    product_info[:description] = FFaker::Lorem.sentence
+    product_info[:category] = FFaker::Lorem.words(3).join
+    product_info[:sku] = FFaker::Lorem.words(2).join
+    product_info[:wholesale] = wholesale_price
+    product_info[:retail] = wholesale_price * 4
+
+    Product.create(product_info)
+
+end
