67bb27e4caf01654950fe44ab8f248aedd7e2bfd
jroers <joe.roers@gmail.com>
2016-01-25 15:35:06 -0800

    Added new method, route and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all
 	end
+
+	def new
+	end
+
+	def create
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   get '/products', to: 'products#index', as: 'products'
+  get '/products/new', to: 'products#new', as: 'new_product'
 end
